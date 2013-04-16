b98161f1362106125aeadcb619fdc62bdd08867a
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 19:30:15 -0400

    getting there with R, now not able to find R libR.so

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ import sys
 
 os.putenv('PATH',os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64', 'R', 'bin:',) + os.environ['PATH'])
 #sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64', 'R', 'lib'))
-print(os.putenv('LD_LIBRARY_PATH',os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64', 'R', 'lib:') + os.environ['LD_LIBRARY_PATH']))
+ld_library_path = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64/R/lib:') + os.environ['LD_LIBRARY_PATH']
+#os.putenv('LD_LIBRARY_PATH',os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64/R/lib:') + os.environ['LD_LIBRARY_PATH'])
+os.putenv('LD_LIBRARY_PATH',ld_library_path)
+print("library var = " + ld_library_path)
 print("library = " + os.environ['LD_LIBRARY_PATH'])
  
 from rpy2.robjects import Formula, r
