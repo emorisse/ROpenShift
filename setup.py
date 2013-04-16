88df5517043bb4939951fe46c9a8804d76fdabcc
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 14:33:20 -0400

    moving rpy2 installation back to build action_hook to try to take care of the path req

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup
 import os
 import sys
 
-sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'] + "/lib64/R/bin"))
-print(sys.path)
-
 setup(name='YourAppName', version='1.0',
       description='OpenShift Python-3.3 Community Cartridge based application',
       author='Erich Morisse', author_email='erich.morisse@gmail.com',
@@ -16,6 +13,5 @@ setup(name='YourAppName', version='1.0',
                         #  'mysql-connector-python',
                         #  'pymongo',
                         #  'psycopg2',
-								'rpy2',
       ],
      )
