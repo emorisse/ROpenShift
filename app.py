87be74b5e083fc726c9b1d6918ed2d6ec0bd82aa
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 19:39:24 -0400

    now not able to find R libR.so. can't set ld_library_path from within python?

--- app.py
+++ app.py
@@ -29,12 +29,13 @@ import os
 import sys
 
 os.putenv('PATH',os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64', 'R', 'bin:',) + os.environ['PATH'])
-#sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64', 'R', 'lib'))
-ld_library_path = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64/R/lib:') + os.environ['LD_LIBRARY_PATH']
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64', 'R', 'lib'))
+print("sys.path = " + sys.path)
+#ld_library_path = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64/R/lib:') + os.environ['LD_LIBRARY_PATH']
 #os.putenv('LD_LIBRARY_PATH',os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'lib64/R/lib:') + os.environ['LD_LIBRARY_PATH'])
-os.putenv('LD_LIBRARY_PATH',ld_library_path)
-print("library var = " + ld_library_path)
-print("library = " + os.environ['LD_LIBRARY_PATH'])
+#os.putenv('LD_LIBRARY_PATH',ld_library_path)
+#print("library var = " + ld_library_path)
+Eprint("library = " + os.environ['LD_LIBRARY_PATH'])
  
 from rpy2.robjects import Formula, r
 from rpy2.robjects.packages import importr
