0d621239ab8deae6ee8fa5478384a41c44981ee7
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 14:20:04 -0400

    adding debugging to setup.py to see why we can't find R

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'] + "/lib64/R/bin"))
+print(sys.path)
 
 setup(name='YourAppName', version='1.0',
       description='OpenShift Python-3.3 Community Cartridge based application',
-      author='Your Name', author_email='ramr@example.org',
+      author='Erich Morisse', author_email='erich.morisse@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 
       #  Uncomment one or more lines below in the install_requires section
