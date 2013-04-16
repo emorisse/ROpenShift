1059365bc1ad1c5cfab4993d398a374abf4905ef
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 17:44:57 -0400

    trying rpy2 install post path setups

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='YourAppName', version='1.0',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=['WebOb',
+                        'rpy2',
                         #  'mysql-connector-python',
                         #  'pymongo',
                         #  'psycopg2',
