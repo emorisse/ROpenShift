f128cf7e41f47108fcc18f346049dca1483567d2
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 17:42:53 -0400

    not clear where these library settings need to go

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup
 
+import os
+import sys
+
+PYCART_DIR = ''.join(['python-', '.'.join(map(str, sys.version_info[:2]))])
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'openshift'))
+virtenv = PYCART_DIR + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+virtualenv = os.path.join(virtenv, 'lib/python2.6/site-packages')
+print virtualenv
+#virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except:
+    pass
+
+
 setup(name='YourAppName', version='1.0',
       description='OpenShift Python-3.3 Community Cartridge based application',
       author='Erich Morisse', author_email='erich.morisse@gmail.com',
