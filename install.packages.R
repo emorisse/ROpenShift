ad21647dee043855684390505a84738e20c530e0
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 21:41:06 -0400

    only install packages that do not already exist

--- install.packages.R
+++ install.packages.R
@@ -3,7 +3,7 @@ pkgs <- read.csv(file.path(directory,"required.R"), header=F)
 f <- NA
 for ( p in pkgs ) {
 	rm(f)
-	f <- library(p)
+	try(f <- library(p))
 	if ( ! exists("f") ) 
 		install.packages(p, repos="http://cran.us.r-project.org")
 }
