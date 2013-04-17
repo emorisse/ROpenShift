99e18a5087b658b681ceb28de17bb2711da055e3
Erich Morisse <erich.morisse@gmail.com>
2013-04-16 21:34:51 -0400

    manipulating environment variables from with R

--- install.packages.R
+++ install.packages.R
@@ -1,5 +1,5 @@
 directory <- Sys.getenv("OPENSHIFT_REPO_DIR");
 pkgs <- read.csv(file.path(directory,"required.R"), header=F)
-for ( p in packages ) {
+for ( p in pkgs ) {
 	install.packages(p, repos="http://cran.us.r-project.org")
 }
